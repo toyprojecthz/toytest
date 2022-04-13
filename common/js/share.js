38191d71d45bf8a323a85cc298a6b436e891bcdb
TANGRAM <tangram@eunsub-iMac.local>
2022-04-13 16:40:41 +0900

    dd

--- common/js/share.js
+++ common/js/share.js
@@ -1,64 +1,64 @@
 function kakaoShare() {
-    Kakao.API.request({
-        url: '/v1/api/talk/friends/message/default/send',
-        data: {
-          receiver_uuids: ['receiver_uuids'],
-          template_object: {
-            object_type: 'feed',
-            content: {
-              title: '카카오톡 링크 4.0',
-              description: '디폴트 템플릿 FEED',
-              image_url:
-                'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-              link: {
-                web_url: 'https://developers.kakao.com',
-                mobile_web_url: 'https://developers.kakao.com',
-              },
+    Kakao.Link.createDefaultButton({
+        container: '#kakao-link-btn',
+        objectType: 'feed',
+        content: {
+            title: '오늘의 디저트',
+            description: '아메리카노, 빵, 케익',
+            imageUrl: 'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+            link: {
+                mobileWebUrl: 'https://developers.kakao.com',
+                androidExecutionParams: 'test',
             },
-            item_content: {
-              profile_text: 'Kakao',
-              profile_image_url: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-              title_image_url: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-              title_image_text: 'Cheese cake',
-              title_image_category: 'Cake',
-              items: [
-                {
-                  item: 'Cake1',
-                  item_op: '1000원',
+        },
+        itemContent: {
+            profileText: 'Kakao',
+            profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+            titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+            titleImageText: 'Cheese cake',
+            titleImageCategory: 'Cake',
+            items: [{
+                    item: 'Cake1',
+                    itemOp: '1000원',
                 },
                 {
-                  item: 'Cake2',
-                  item_op: '2000원',
+                    item: 'Cake2',
+                    itemOp: '2000원',
                 },
                 {
-                  item: 'Cake3',
-                  item_op: '3000원',
+                    item: 'Cake3',
+                    itemOp: '3000원',
                 },
                 {
-                  item: 'Cake4',
-                  item_op: '4000원',
+                    item: 'Cake4',
+                    itemOp: '4000원',
                 },
                 {
-                  item: 'Cake5',
-                  item_op: '5000원',
+                    item: 'Cake5',
+                    itemOp: '5000원',
                 },
-              ],
-              sum: 'Total',
-              sum_op: '15000원',
+            ],
+            sum: 'Total',
+            sumOp: '15000원',
+        },
+        social: {
+            likeCount: 10,
+            commentCount: 20,
+            sharedCount: 30,
+        },
+        buttons: [{
+                title: '웹으로 이동',
+                link: {
+                    mobileWebUrl: 'https://developers.kakao.com',
+                },
             },
-            social: {
-              like_count: 100,
-              comment_count: 200,
+            {
+                title: '앱으로 이동',
+                link: {
+                    mobileWebUrl: 'https://developers.kakao.com',
+                },
             },
-            button_title: '바로 확인',
-          },
-        },
-        success: function(response) {
-          console.log(response);
-        },
-        fail: function(error) {
-          console.log(error);
-        },
-      });
-      
+        ]
+    });
+
 }
