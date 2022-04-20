7889539296993c6b292465eb47aa1118b75c3561
TANGRAM <tangram@eunsub-iMac.local>
2022-04-20 17:47:42 +0900

    dd

--- common/js/geumjjock-share.js
+++ common/js/geumjjock-share.js
@@ -10,47 +10,28 @@ function setShare() {
     const shareDes = infoList[resultAlt].title;
     const shareImage = '"' + url + "img/image-" + resultAlt + ".jpg" + '"';
     const shareURL = '"' + url + 'result/result-' + resultAlt + '.html' + '"';
-    Kakao.Link.sendDefault({
-        objectType: 'feed', // 메시지 형식 : 피드 타입
-        content: {
-          title: shareTitle,
-          description: shareDes,
-          imageUrl: "https://toyprojecthz.github.io/toytest/friendPersona/img/image-5.jpg",
-          link: {
-            webUrl: url,
-            mobileWebUrl: url,
-          },
-        },
-        buttons: [
-          {
-            title: '나도 테스트 해보기', // 버튼 이름
-            link: {
-              webUrl: shareURL,
-              mobileWebUrl: shareURL,
-            },
-          },
-        ],
-      });    
-    // Kakao.Link.sendDefault({
-    //     objectType: 'feed',
-    //     content: {
-    //         title: shareTitle,
-    //         description: shareDes,
-    //         imageUrl: "https://toyprojecthz.github.io/toytest/friendPersona/img/image-5.jpg",
-    //         // link: {
-    //         //     mobileWebUrl: shareURL,
-    //         //     webUrl: shareURL,
-    //         // },
-    //     },
 
-    //     buttons: [{
-    //         title: '결과 확인하기',
-    //         link: {
-    //             mobileWebUrl: shareURL,
-    //             webUrl: shareURL,
-    //         },
-    //     }]
-    // });
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: "https://toyprojecthz.github.io/toytest/friendPersona/img/image-5.jpg",
+            link: {
+                webUrl: url,
+                mobileWebUrl: url,
+            },
+        },
+
+        buttons: [{
+            title: '결과 확인하기',
+            link: {
+                webUrl: shareURL,
+                mobileWebUrl: shareURL,
+            },
+        }]
+    });
 
 
 }
