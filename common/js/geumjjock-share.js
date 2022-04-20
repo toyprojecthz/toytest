8ddc86113ed5a6c47b70dae8951d57827da6d03c
TANGRAM <tangram@eunsub-iMac.local>
2022-04-20 17:26:50 +0900

    dd

--- common/js/geumjjock-share.js
+++ common/js/geumjjock-share.js
@@ -17,16 +17,16 @@ function setShare() {
             description: shareDes,
             imageUrl: shareImage,
             link: {
-                mobileWebUrl: shareURL,
-                webUrl: shareURL,
+                mobileWebUrl: "https://toyprojecthz.github.io/toytest/friendPersona/",
+                webUrl: "https://toyprojecthz.github.io/toytest/friendPersona/",
             },
         },
 
         buttons: [{
             title: '결과 확인하기',
             link: {
-                mobileWebUrl: shareURL,
-                webUrl: shareURL,
+                mobileWebUrl: "https://toyprojecthz.github.io/toytest/friendPersona/",
+                webUrl: "https://toyprojecthz.github.io/toytest/friendPersona/",
             },
         }]
     });
