fd58022d2ce2c2c148e3315207548a959253d202
TANGRAM <tangram@eunsub-iMac.local>
2022-04-21 09:26:54 +0900

    shr fixed

--- common/js/geumjjock-share.js
+++ common/js/geumjjock-share.js
@@ -1,6 +1,5 @@
 function setShare() {
-    // const url = "https://toyprojecthz.github.io/toytest/friendPersona/"
-    const url = "https://www.naver.com/"
+    const url = 'https://toyprojecthz.github.io/toytest/friendPersona/'
     var resultImg = document.querySelector("#resultImg");
     var resultAlt = resultImg.firstElementChild.alt;
     console.log(resultAlt)
@@ -9,8 +8,7 @@ function setShare() {
     const shareTitle = "금쪽이 테스트!"
     const shareDes = infoList[resultAlt].title;
     const shareImage = '"' + url + "img/image-" + resultAlt + ".jpg" + '"';
-    // const shareURL = '"' + url + 'result/result-' + resultAlt + '.html' + '"';
-    // const shareURL = url;
+    const shareURL = '"' + url + 'result/result-' + resultAlt + '.html' + '"';
 
 
     Kakao.Link.sendDefault({
@@ -20,16 +18,16 @@ function setShare() {
             description: shareDes,
             imageUrl: shareImage,
             link: {
-                webUrl: url,
-                mobileWebUrl: url,
+                webUrl: '"' + url + '"',
+                mobileWebUrl: '"' + url + '"',
             },
         },
 
         buttons: [{
             title: '결과 확인하기',
             link: {
-                webUrl: url,
-                mobileWebUrl: url,
+                webUrl: shareURL,
+                mobileWebUrl: shareURL,
             },
         }]
     });
