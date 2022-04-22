3ab5a56535ca84b77deecc86cdb5cff516799a5f
TANGRAM <tangram@eunsub-iMac.local>
2022-04-22 14:57:01 +0900

    counter add

--- common/js/counter.js
+++ common/js/counter.js
@@ -1,19 +1,3 @@
-$(document).ready(function () {
-    var defaultNumb = 2982
-    var n = localStorage.getItem('on_load_counter');
-
-    if (n === null) {
-        n = 0;
-    }
-    n++;
-
-    localStorage.setItem("on_load_counter", n);
-
-    nums = n.toString().split('').map(Number);
-
-    var countContainer = document.getElementById("CounterVisitor");
-    countContainer.innerHTML = '';
-    for (var i of nums) {
-        countContainer.innerHTML = defaultNumb + i;
-    }
-});
+function websiteVisits(response) {
+    document.querySelector("#CounterVisitor").textContent = response.value;
+}
