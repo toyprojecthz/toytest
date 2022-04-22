a7af6d0d041f449edb401600bbe4a2246a152a2a
TANGRAM <tangram@eunsub-iMac.local>
2022-04-22 14:21:47 +0900

    d

--- common/js/counter.js
+++ common/js/counter.js
@@ -1,9 +1,18 @@
 var n = localStorage.getItem('on_load_counter');
+
 if (n === null) {
     n = 0;
 }
+n++;
+console.log(n)
 
-// localStorage.setItem("on_load_counter", n);
+localStorage.setItem("on_load_counter", n);
 
-var visitorContainer = document.getElementById('CounterVisitor')
-visitorContainer.innerHTML = n;
+nums = n.toString().split('').map(Number);
+
+var countContainer = document.getElementById("CounterVisitor");
+countContainer.innerHTML = '';
+for (var i of nums) {
+    countContainer.innerHTML += i;
+    console.log(n)
+}
